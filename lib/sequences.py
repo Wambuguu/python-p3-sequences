55750a76e2b10241e589d14eecc0d59e885c17a3
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-21 10:36:45 +0300

    add

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    fibonacci_sequence = [0] * length
+    if length > 0:
+        fibonacci_sequence[0] = 0
+    if length > 1:
+        fibonacci_sequence[1] = 1
+
+    for i in range(2, length):
+        fibonacci_sequence[i] = fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]
+
+    print(fibonacci_sequence)
